3cc0ed3a87f4cd74c02f8d14b3db14d49121ff96
Marty Sloan <marty@karmatek.io>
2025-08-09 16:35:36 -0400

    Realize that vendorConfig is already on the phone model

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -11,35 +11,10 @@ class Phone extends Model
 {
     protected $guarded = [];
 
-    protected $casts = [
-        'expansion_modules' => 'array',
-        'vendor_config' => 'array',
-        'enable_extension_mobility' => 'boolean',
-    ];
-
     public function ucm(): BelongsTo
     {
         return $this->belongsTo(Ucm::class);
     }
-
-    public function lines(): BelongsToMany
-    {
-        return $this->belongsToMany(Line::class, 'device_line')
-            ->withPivot([
-                'index',
-                'dirn',
-                'display',
-                'display_ascii',
-                'e164_alt_num',
-                'external_phone_number_mask',
-                'max_num_calls',
-                'busy_trigger',
-                'ring_settings',
-            ])
-            ->withTimestamps();
-    }
-
-    // Vendor config will be embedded on this model (e.g., vendor_config xml/json)
 
     public static function storeUcmDetails(array $phone, Ucm $ucm): void
     {
