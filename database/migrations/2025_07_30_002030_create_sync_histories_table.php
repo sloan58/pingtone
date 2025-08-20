2016cf761da2c2eae42fc19b3b480d1b3259c4da
Marty Sloan <marty@karmatek.io>
2025-08-20 08:06:27 -0400

    Adjust sync_histories migration

--- database/migrations/2025_07_30_002030_create_sync_histories_table.php
+++ database/migrations/2025_07_30_002030_create_sync_histories_table.php
@@ -12,13 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('sync_histories', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('syncable');
-            $table->timestamp('sync_start_time');
-            $table->timestamp('sync_end_time')->nullable();
-            $table->enum('status', ['syncing', 'completed', 'failed'])->default('syncing');
-            $table->text('error')->nullable();
-            $table->timestamps();
+            //
         });
     }
 
